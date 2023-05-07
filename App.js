fb134b85286ebb3012f8fa07e14d248d1683306b
Sandro2906 <132219558+Sandro2906@users.noreply.github.com>
2023-05-07 19:24:36 +0200

    FLATLIST
    Example of useing  a flatlist!

--- App.js
+++ App.js
@@ -1,10 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ImageBackground } from 'react-native';
+
+const DATACar = [
+  {
+    id:1,
+    name:'BMW',
+    price:'100$',
+    image: require('./assets/pexels-maria-geller-2127039.jpg'),
+  },  
+  {
+  id:2,
+  name:'BMW',
+  price:'102$',
+  image: require('./assets/pexels-zhuzichun-14292717.jpg'),
+  },  
+];
+
+const Cars = ({name, price, image}) => {
+  return(
+    <View style={styles.all}>
+      <ImageBackground source={image} style={styles.image}/>
+      <View style={styles.views}>
+        <Text>{name}</Text>
+        <Text>{price}</Text>
+      </View>
+  </View>
+  )
+};
 
 export default function App() {
+
+  const renderItem = ({item}) => (
+    <Cars name={item.name} 
+    price={item.price} 
+    image={item.image} />
+  )
+
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <FlatList 
+      data={DATACar}
+      keyExtractor={(item) => item.id}
+      renderItem={renderItem}
+      />
+     
+    
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +56,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  
   },
+  image:{
+    width:400,
+    height:600,
+    
+  },
+  all:{
+    width:'100%',
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  views:{
+    width:'100%',
+    height:100,
+    justifyContent:'center',
+    alignItems:'center',
+    position:'absolute'
+  }
 });
